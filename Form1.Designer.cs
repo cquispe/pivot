d8dd691b72ef8417405bd39c68cb03ae3f601d51
christian quispe <cquispe@hunterlojack.com>
2020-12-30 12:28:24 -0500

    commit inicial

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace pivot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnNumber = new System.Windows.Forms.Button();
+            this.btnString = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.tbxInput = new System.Windows.Forms.TextBox();
+            this.tbxPivot = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnNumber
+            // 
+            this.btnNumber.Location = new System.Drawing.Point(11, 143);
+            this.btnNumber.Name = "btnNumber";
+            this.btnNumber.Size = new System.Drawing.Size(75, 23);
+            this.btnNumber.TabIndex = 0;
+            this.btnNumber.Text = "&Number";
+            this.btnNumber.UseVisualStyleBackColor = true;
+            this.btnNumber.Click += new System.EventHandler(this.btnNumber_Click);
+            // 
+            // btnString
+            // 
+            this.btnString.Location = new System.Drawing.Point(93, 143);
+            this.btnString.Name = "btnString";
+            this.btnString.Size = new System.Drawing.Size(75, 23);
+            this.btnString.TabIndex = 1;
+            this.btnString.Text = "&String";
+            this.btnString.UseVisualStyleBackColor = true;
+            this.btnString.Click += new System.EventHandler(this.btnString_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(370, 143);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // tbxInput
+            // 
+            this.tbxInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxInput.Location = new System.Drawing.Point(12, 12);
+            this.tbxInput.Multiline = true;
+            this.tbxInput.Name = "tbxInput";
+            this.tbxInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbxInput.Size = new System.Drawing.Size(432, 125);
+            this.tbxInput.TabIndex = 3;
+            // 
+            // tbxPivot
+            // 
+            this.tbxPivot.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxPivot.Location = new System.Drawing.Point(12, 172);
+            this.tbxPivot.Multiline = true;
+            this.tbxPivot.Name = "tbxPivot";
+            this.tbxPivot.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbxPivot.Size = new System.Drawing.Size(432, 216);
+            this.tbxPivot.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 400);
+            this.Controls.Add(this.tbxPivot);
+            this.Controls.Add(this.tbxInput);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnString);
+            this.Controls.Add(this.btnNumber);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Pivot";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNumber;
+        private System.Windows.Forms.Button btnString;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox tbxInput;
+        private System.Windows.Forms.TextBox tbxPivot;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace pivot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNumber_Click(object sender, EventArgs e)
+        {
+            Pivot("N");
+        }
+                
+        private void btnString_Click(object sender, EventArgs e)
+        {
+            Pivot("S");
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            tbxInput.Clear();
+            tbxPivot.Clear();
+            tbxInput.Focus();
+        }
+
+
+        private void Pivot(string characterType)
+        {
+            string resultPivot = "";
+
+            if (characterType == "N")
+            {
+                for (int i = 0; i < tbxInput.Lines.Length; i++)
+                {
+                    if (tbxInput.Lines[i].Trim().Length > 0)
+                    {
+                        resultPivot += $"{tbxInput.Lines[i]},";
+                    }
+                }
+            } 
+            else
+            {
+                for (int i = 0; i < tbxInput.Lines.Length; i++)
+                {
+                    if (tbxInput.Lines[i].Trim().Length > 0)
+                    {
+                        resultPivot += $"'{tbxInput.Lines[i]}',";
+                    }
+                }
+            }
+
+            resultPivot = resultPivot.TrimEnd(',');
+
+            tbxPivot.Focus();
+            tbxPivot.Text = resultPivot;
+            tbxPivot.SelectAll();
+            tbxPivot.Copy();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ActiveControl = tbxInput;
+        }
+    }
+}
